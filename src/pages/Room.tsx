1c87c2429eaaf64e8baf6284582f72a17bac1806
evencoding <evencoding@gmail.com>
2022-04-28 23:44:18 +0900

    Room set

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+const Main = styled.div``;
+const Timer = styled.div``;
+const UserCam = styled.div``;
+const Chat = styled.div``;
+
 function Room() {
-  return <div>Room</div>;
+  return (
+    <Wrapper>
+      <Main>
+        <Timer>Profile</Timer>
+        <UserCam>
+          <div></div>
+        </UserCam>
+      </Main>
+      <Chat></Chat>
+    </Wrapper>
+  );
 }
 
 export default Room;
